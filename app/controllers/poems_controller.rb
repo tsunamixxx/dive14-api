91e0d8d3cd6ede64f850cfbd9d806027e6944e5e
津波忍 <tsuna@tsunamishinobu-no-MacBook-Air.local>
2017-10-01 23:39:44 +0900

    dive14-02-01

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,4 +3,10 @@ class PoemsController < ApplicationController
     @poems = Poem.all
     render json: @poems
   end
+
+  def show
+    @poem = Poem.find(params[:id])
+    render json: @poem
+  end
+
 end
